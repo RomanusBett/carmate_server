08c374491b192fc3e0b9b3a12b611c12224609d4
Romie Bett <bett@Betts-MacBook-Air.local>
2022-07-07 13:02:00 +0300

    added a migrate file

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://plrvnfnhmkfxmq:ebca9231c476d7d75eadf248e024f74f65e319f416d6357e6cde977c7b2ea790@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d238lfqrimedtp'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bett:2019@localhost/bett'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bett:2019@localhost/bett'
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
