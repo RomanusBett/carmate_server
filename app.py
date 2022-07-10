b7a0c3f1bb7bd23b909c85916e2d949ba8f4e0d3
Romie Bett <bett@Betts-MacBook-Air.local>
2022-07-11 01:06:28 +0300

    cors origin addition

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ class User(db.Model):
         return "<user %r>" % self.email
     
 @app.route("/register", methods=['POST'])
-@cross_origin()
 def register_user():
    email = request.json["email"]
    password  = request.json["password"]
@@ -62,7 +61,6 @@ def register_user():
        })
 
 @app.route("/about")
-@cross_origin()
 def get_user():
     user_id=session.get("user_id")
 
@@ -75,7 +73,6 @@ def get_user():
     })
 
 @app.route("/login", methods=['POST'])
-@cross_origin()
 def login_user():
     email = request.json["email"]
     password  = request.json["password"]
@@ -97,7 +94,6 @@ def login_user():
     })
 
 @app.route("/@me")
-@cross_origin()
 def get_current_user(): 
     user_id = session.get("user_id")
 
@@ -112,7 +108,6 @@ def get_current_user():
 
 
 @app.route('/logout', methods=['POST'])
-@cross_origin()
 def logout_user():
     session.pop("user_id")
     return "200"
